5fe74f49799d9dbb5dc6faea7ff7630d9c7a16c3
tophotoffer <taishidiannao2024@gmail.com>
2025-06-23 23:49:19 +0800

    Delete click directory

--- click/5/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://www.amazon.de/Timekettle-Dolmetscher-Hub-Zwei-Wege-%C3%9Cbersetzer-Ohrh%C3%B6rer-%C3%9Cbersetzungsger%C3%A4t-multilinguale/dp/B0CRH6KX3B");
-exit;
-?>
